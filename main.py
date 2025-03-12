fab8183f6e544951434467fc0d1d84a02bc9a879
mevrse <mevrse@protonmail.com>
2025-03-12 19:18:27 -0300

    Start website

--- main.py
+++
@@ -1,7 +0,0 @@
-if __name__=='__main__':
-    try:
-        from brain.interface import interface
-    except Exception as e:
-        print(f"Erro ao iniciar o programa: {e}")
-    finally:
-        interface()
